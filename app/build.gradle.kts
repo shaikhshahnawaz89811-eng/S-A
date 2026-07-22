222e35e400bffc7e909a2ad1d574f0a8399df126
Your Name <your@email.com>
2026-07-22 17:56:47 +0530

    Add Material Components dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,6 +93,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.google.material)
     implementation(libs.androidx.material.icons)
     implementation(libs.navigation.compose)
 
